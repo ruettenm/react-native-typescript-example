69832bcfea06934bc0e181fef72076188572305b
Matthias Rütten <matthias.ruetten@codecentric.de>
2018-10-12 15:12:03 +0200

    add second page

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Home from './src/screens/Home'
+import Login from './src/screens/Login'
 import { Navigation } from 'react-native-navigation'
 
 Navigation.registerComponent(`HomeScreen`, () => Home)
+Navigation.registerComponent(`LoginScreen`, () => Login)
 
 Navigation.events().registerAppLaunchedListener(() => {
     Navigation.setRoot({
